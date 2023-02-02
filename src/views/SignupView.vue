fea49f0c2ff29e69bcd5bab44a2e93d507ec2a6e
Luca <luca.dematte2020@gmail.com>
2023-02-03 00:55:22 +0100

    Fix reset foto quando si esce dal dialog

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -3,11 +3,11 @@ import { ref, onMounted } from "vue";
 import { loggedUser, setLoggedUser, clearLoggedUser } from "../states/loggedUser.js";
 import { reactive } from "vue";
 
-let x,y;
-let oldx,oldy,premuto;
+let x, y;
+let oldx, oldy, premuto;
 let listener;
-let ratio=1;
-let dragging=false;
+let ratio = 1;
+let dragging = false;
 
 let cambio;
 
@@ -15,60 +15,60 @@ onMounted(() => {
     //TODO potrei controllare se l'utente è loggato, e in tal caso mandarlo al profilo
     console.log("registrazione mounted");
 
-    const defaultImage = document.getElementById('defaultPic');
-    defaultImage.onload=()=>{
-        const canvas = document.getElementById('canvas');
-        canvas.setAttribute('width', defaultImage.width);
-        canvas.setAttribute('height', defaultImage.height);
-        canvas.getContext("2d").drawImage(defaultImage,0,0);
+    const defaultImage = document.getElementById("defaultPic");
+    defaultImage.onload = () => {
+        const canvas = document.getElementById("canvas");
+        canvas.setAttribute("width", defaultImage.width);
+        canvas.setAttribute("height", defaultImage.height);
+        canvas.getContext("2d").drawImage(defaultImage, 0, 0);
     };
     var context = canvas.getContext("2d");
 
-    ['mousedown','touchstart'].forEach(eventName=>{
-        canvas.addEventListener(eventName,e=>{
-            premuto=true;
+    ["mousedown", "touchstart"].forEach((eventName) => {
+        canvas.addEventListener(eventName, (e) => {
+            premuto = true;
             oldx = e.x || e.changedTouches[0].clientX;
             oldy = e.y || e.changedTouches[0].clientY;
         });
     });
 
-    ['mouseup','touchend'].forEach(eventName=>{
-        document.addEventListener(eventName,e=>{
-            premuto=false;
-            dragging=false;
+    ["mouseup", "touchend"].forEach((eventName) => {
+        document.addEventListener(eventName, (e) => {
+            premuto = false;
+            dragging = false;
         });
     });
 
-    cambio = ()=>{
-        const input = document.getElementById('fileInput')
+    cambio = () => {
+        const input = document.getElementById("fileInput");
         const file = input.files[0];
-        if (!file)
+        if (!file) {
+            resetPhoto();
             return;
+        }
         const url = URL.createObjectURL(file);
         const img = document.getElementById("img");
-        img.onload=()=>{
-            if (img.width<img.height)
-                ratio = canvas.height/img.width;
-            else
-                ratio = canvas.height/img.height;
+        img.onload = () => {
+            if (img.width < img.height) ratio = canvas.height / img.width;
+            else ratio = canvas.height / img.height;
 
-            const newWidth = img.width*ratio
-            const newHeight = img.height*ratio;
-            
-            const minWidth = canvas.width-newWidth;
-            const minHeight = canvas.height-newHeight;
-				
-            x = minWidth/2;
-            y = minHeight/2;
+            const newWidth = img.width * ratio;
+            const newHeight = img.height * ratio;
 
-            context.clearRect(0,0,canvas.width,canvas.height);
-            context.drawImage(img, x, y, newWidth,newHeight);
+            const minWidth = canvas.width - newWidth;
+            const minHeight = canvas.height - newHeight;
+
+            x = minWidth / 2;
+            y = minHeight / 2;
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(img, x, y, newWidth, newHeight);
 
             if (listener)
-                ['mousemove','touchmove'].forEach(eventName=>{
-                    canvas.removeEventListener(eventName, listener)
+                ["mousemove", "touchmove"].forEach((eventName) => {
+                    canvas.removeEventListener(eventName, listener);
                 });
-            listener = e => {
+            listener = (e) => {
                 e.preventDefault();
                 if (!premuto) return;
                 dragging = true;
@@ -76,25 +76,25 @@ onMounted(() => {
                 const ex = e.x || e.changedTouches[0].clientX;
                 const ey = e.y || e.changedTouches[0].clientY;
 
-                const deltax = ex-oldx;
-                const deltay = ey-oldy;
+                const deltax = ex - oldx;
+                const deltay = ey - oldy;
 
-                x = Math.min(0, Math.max(minWidth, x+deltax));
-                y = Math.min(0, Math.max(minHeight, y+deltay));
+                x = Math.min(0, Math.max(minWidth, x + deltax));
+                y = Math.min(0, Math.max(minHeight, y + deltay));
 
-                context.clearRect(0,0,canvas.width,canvas.height);
-                context.drawImage(img, x,y, newWidth,newHeight);
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                context.drawImage(img, x, y, newWidth, newHeight);
 
-                oldx=ex;
-                oldy=ey;
-            }
+                oldx = ex;
+                oldy = ey;
+            };
 
-            ['mousemove','touchmove'].forEach(eventName=>{
+            ["mousemove", "touchmove"].forEach((eventName) => {
                 canvas.addEventListener(eventName, listener);
             });
-        }
-        img.setAttribute('src', url);
-    }
+        };
+        img.setAttribute("src", url);
+    };
 });
 
 let signupStatus = reactive({
@@ -139,47 +139,46 @@ function signup() {
 
         // prende l'immagine originale e la ritaglia in base a come e stata posizionata nel canvas dall'utente
         // questo viene fatto passando per un altro canvas hidden
-        const canvas = document.getElementById('canvas2');
-        const img = document.getElementById('img');
+        const canvas = document.getElementById("canvas2");
+        const img = document.getElementById("img");
 
-        canvas.setAttribute('width', Math.min(img.width, img.height));
-        canvas.setAttribute('height', Math.min(img.width, img.height));
+        canvas.setAttribute("width", Math.min(img.width, img.height));
+        canvas.setAttribute("height", Math.min(img.width, img.height));
 
-        const context = canvas.getContext('2d');
-        context.clearRect(0,0,canvas.width, canvas.height);
-        context.drawImage(img, x/ratio,y/ratio)
+        const context = canvas.getContext("2d");
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(img, x / ratio, y / ratio);
 
-        canvas.toBlob(blob=>{
-            if (document.getElementById('fileInput').files[0])  
-                signupData.append("foto", new File([blob],'profilePicture.png'));
+        canvas.toBlob((blob) => {
+            if (document.getElementById("fileInput").files[0])
+                signupData.append("foto", new File([blob], "profilePicture.png"));
 
             fetch(API_USER_URL + "/registrazione", {
                 method: "POST",
                 body: signupData,
             }).then((resp) =>
                 resp
-                .json()
-                .then(function (data) {
-                    console.log(resp);
-                    console.log(data);
-
-                    if (!resp.ok) {
-                        console.error(data.message);
-                    } else {
+                    .json()
+                    .then(function (data) {
+                        console.log(resp);
                         console.log(data);
-                        //TODO dire all'utente di guardare la sua casella di posta
-                        signupStatus.status = true;
-                    }
 
-                    //emit("login", loggedUser);
-                    return;
-                })
-                .catch((error) => console.error(error))
+                        if (!resp.ok) {
+                            console.error(data.message);
+                        } else {
+                            console.log(data);
+                            //TODO dire all'utente di guardare la sua casella di posta
+                            signupStatus.status = true;
+                        }
+
+                        //emit("login", loggedUser);
+                        return;
+                    })
+                    .catch((error) => console.error(error))
             );
         });
     }
 }
-
 
 //simula un click sul campo nascosto per caricare la foto
 function selectPhoto() {
@@ -189,9 +188,14 @@ function selectPhoto() {
 }
 
 function resetPhoto() {
-    document.getElementById('fileInput').value="";
-    document.getElementById('img').setAttribute('src', document.getElementById('defaultPic').getAttribute('src'));
-    document.getElementById('canvas').getContext('2d').drawImage(document.getElementById('defaultPic'),0,0);
+    document.getElementById("fileInput").value = "";
+    document
+        .getElementById("img")
+        .setAttribute("src", document.getElementById("defaultPic").getAttribute("src"));
+    document
+        .getElementById("canvas")
+        .getContext("2d")
+        .drawImage(document.getElementById("defaultPic"), 0, 0);
 }
 </script>
 
@@ -208,12 +212,19 @@ function resetPhoto() {
                     <table id="image-section">
                         <tr>
                             <td rowspan="2">
-                                <img src="@/assets/profile-default.png" id='defaultPic' class="profile-picture" hidden />
+                                <img
+                                    src="@/assets/profile-default.png"
+                                    id="defaultPic"
+                                    class="profile-picture"
+                                    hidden />
                                 <canvas id="canvas2" hidden></canvas>
-                                <img id="img" hidden>
+                                <img id="img" hidden />
 
-                                <canvas id="canvas" class="profile-picture" @mouseup="selectPhoto()"></canvas>
-                                    
+                                <canvas
+                                    id="canvas"
+                                    class="profile-picture"
+                                    @mouseup="selectPhoto()"></canvas>
+
                                 <input
                                     type="file"
                                     id="fileInput"
