123b6a7c6954b427be0cde16e0bdee543c19402b
Diego <diego.oniarti@studenti.unitn.it>
2023-02-03 19:12:53 +0100

    Aggiunte le funzionalità base per la pagina oratorio

---
+++ src/components/CardServizio.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps(['servizio']);
+const servizio = props.servizio;
+
+const urlFoto = import.meta.env.VITE_BACKEND+servizio.URLfoto;
+
+</script>
+
+<template>
+    <fieldset>
+        <legend>{{ servizio.nome }}</legend>
+        <table>
+            <tr>
+                <td>tipologia:</td>
+                <td>{{ servizio.tipologia }}</td>
+            </tr>
+            <tr>
+                <td>prezzo:</td>
+                <td>{{servizio.prezzoIniziale}} + {{ servizio.prezzoOra }}/ora</td>
+            </tr>
+        </table>
+        <p>{{ servizio.descrizione }}</p>
+        <img :src="urlFoto" :alt="servizio.nome" width="100" height="100">
+    </fieldset>
+</template>
--- src/views/OratorioView.vue
+++ src/views/OratorioView.vue
@@ -1,10 +1,31 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import CardSpazio from '../components/CardSpazio.vue';
+
+const spazi = ref([]);
+
+const HOST = import.meta.env.VITE_BACKEND;
+fetch(`${HOST}/spazio/`)
+.then(res=>res.json())
+.then(data=>{
+    spazi.value = data;
+})
+.catch(error=>{
+    console.log(error)
+})
+
+</script>
+
 
 <template>
     <main>
         <h2>Sei nella pagina oratorio</h2>
         <!-- TODO aggiungere contenuti della home -->
-    </main>
+        <div v-for="spazio in spazi">
+            <CardSpazio :spazio="spazio"></CardSpazio>
+        </div>
+
+</main>
 </template>
 
 <style>
