d9242da1655eb7d55cdc2e4753694f296cf33d9c
Diego <diego.oniarti@studenti.unitn.it>
2023-02-04 19:56:31 +0100

    aggiunto selettore delle pagine per  la pagina oratorio
    I'evento click dei bottoni non viene triggerato. Se nel server ci sono 10*n spazi, è presente una pagina vuota

--- src/views/OratorioView.vue
+++ src/views/OratorioView.vue
@@ -3,16 +3,31 @@ import { ref } from 'vue';
 import CardSpazio from '../components/CardSpazio.vue';
 
 const spazi = ref([]);
+const max = 10;
+const pagina = ref(0);
 
 const HOST = import.meta.env.VITE_BACKEND;
-fetch(`${HOST}/spazio/`)
-.then(res=>res.json())
-.then(data=>{
-    spazi.value = data;
-})
-.catch(error=>{
-    console.log(error)
-})
+
+const getSpazi = ()=>{
+    fetch(`${HOST}/spazio/?start=${pagina.value*max}&count=${max}`)
+    .then(res=>res.json())
+    .then(data=>{
+        spazi.value = data;
+    })
+    .catch(error=>{
+        console.log(error)
+    });
+}
+getSpazi();
+
+const avanti = ()=>{
+    pagina.value++;
+    getSpazi()
+};
+const indietro = ()=>{
+    pagina.value--;
+    getSpazi()
+};
 
 </script>
 
@@ -21,11 +36,17 @@ fetch(`${HOST}/spazio/`)
     <main>
         <h2>Sei nella pagina oratorio</h2>
         <!-- TODO aggiungere contenuti della home -->
-        <div v-for="spazio in spazi">
+        <div v-for="spazio in spazi" v-bind:key="spazio._id">
             <CardSpazio :spazio="spazio"></CardSpazio>
         </div>
 
-</main>
+        <!--bottoni di navigazione-->
+        <div v-if="pagina>0 || spazi.length==max">
+            <button id="indietro" v-if="pagina>0" @click="indietro">«{{ pagina }}</button>
+            <p>{{ pagina+1 }}</p>
+            <button id="avanti" v-if="spazi.length<=max" @click="avanti">{{ pagina+2 }}»</button>
+        </div>
+    </main>
 </template>
 
 <style>
